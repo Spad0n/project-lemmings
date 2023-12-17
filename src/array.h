813f1dbd3dda96f1ae051396cdf5133e450e4367
Spad0n <spad0n.chairmanship@simplelogin.co>
2023-12-17 19:07:56 +0100

    ajout de commentaire dans main et array

--- src/array.h
+++ src/array.h
@@ -5,69 +5,110 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
-// old implementation
-//#define array_meta(array)
-//    (&CAST(Array*, (array))[-1])
-
-/*
- * *(arr + i) == arr[i]
+/**
+ * @brief Structure représentant un tableau dynamique.
  */
-
 typedef struct {
-    size_t size;
-    size_t capacity;
+    size_t size;     /**< Nombre d'éléments actuellement dans le tableau. */
+    size_t capacity; /**< Capacité totale du tableau. */
 } Array;
 
-#define ASSERT(expression) \
-    CAST(void, (expression) ? CAST(void, 0) : report_assertion(#expression, __FILE__, __LINE__))
+#define CAST(T, expr) ((T)(expr)) /**< Macro pour effectuer une conversion de type. */
 
-#define CAST(T, expr) ((T)(expr))
-
+/**
+ * @brief Macro pour obtenir les métadonnées du tableau.
+ * 
+ * Cette macro permet d'obtenir un pointeur vers les métadonnées du tableau,
+ * qui sont stockées juste avant le début de la zone allouée pour le tableau.
+ */
 #define array_meta(array) \
     (CAST(Array*, (array)) - 1)
 
+/**
+ * @brief Macro pour tenter d'agrandir le tableau.
+ * 
+ * Cette macro vérifie si le tableau a assez de capacité pour accueillir
+ * une taille supplémentaire spécifiée. Si ce n'est pas le cas, elle appelle
+ * la fonction array_grow pour agrandir le tableau.
+ */
 #define array_try_grow(array, size_) \
     (((array) && array_meta(array)->size + (size_) < array_meta(array)->capacity) \
      ? true \
      : array_grow(CAST(void **, &(array)), (size_), sizeof *(array)))
 
+/**
+ * @brief Macro pour obtenir la taille actuelle du tableau.
+ */
 #define array_size(array) \
     ((array) ? array_meta(array)->size : 0)
 
+/**
+ * @brief Macro pour obtenir la capacité actuelle du tableau.
+ */
 #define array_capacity(array) \
     ((array) ? array_meta(array)->capacity : 0)
 
+/**
+ * @brief Macro pour étendre le tableau.
+ * 
+ * Cette macro tente d'agrandir le tableau de la taille spécifiée.
+ */
 #define array_expand(array, size_) \
     (array_try_grow((array), (size_)) \
      ? (array_meta(array)->size += (size_), true) \
      : false)
 
+/**
+ * @brief Macro pour ajouter un élément à la fin du tableau.
+ * 
+ * Cette macro tente d'agrandir le tableau de 1, puis ajoute la valeur à la fin.
+ */
 #define array_push(array, value) \
     (array_try_grow((array), 1) \
      ? ((array)[array_meta(array)->size++] = (value), true) \
      : false)
 
+/**
+ * @brief Macro pour supprimer le premier élément du tableau.
+ */
 #define array_pop_front(array) \
     (array_size(array) ? \
      (memmove((array), &(array)[1], sizeof *(array) * (array_meta(array)->size - 1)), \
       array_meta(array)->size--, true) \
      : false)
 
+/**
+ * @brief Macro pour supprimer le dernier élément du tableau.
+ */
 #define array_pop_last(array) \
     (array_size(array) ? \
      (array_resize((array), array_size(array) - 1), true) \
      : false)
 
+/**
+ * @brief Macro pour supprimer l'élément à un index donné du tableau.
+ */
 #define array_pop_at(array, index) \
     (array_size(array) && (index) < array_size(array) ?	\
      (memmove((array) + (index), &(array)[(index) + 1], sizeof *(array) * (array_meta(array)->size - (index) - 1)), \
       array_meta(array)->size--, true) \
      : false)
 
+/**
+ * @brief Macro pour libérer la mémoire associée au tableau.
+ */
 #define array_free(array) \
     array_delete((void*)array)
 
+/**
+ * @brief Macro pour redimensionner le tableau.
+ * 
+ * Cette macro redimensionne le tableau à la taille spécifiée.
+ * Si le tableau est déjà assez grand, il ajuste simplement la taille actuelle.
+ * Sinon, il tente d'agrandir le tableau.
+ */
 #define array_resize(array, size_) \
     ((array) \
      ? (array_meta(array)->size >= (size_) \
@@ -77,32 +118,38 @@ typedef struct {
 	? (array_meta(array)->size = (size_), true) \
 	: false))
 
+/**
+ * @brief Macro pour obtenir le dernier élément du tableau.
+ */
 #define array_last(array) \
     ((array)[array_size(array) - 1])
 
+/**
+ * @brief Macro pour vider le tableau (définir la taille à zéro).
+ */
 #define array_clear(array) \
     (void)((array) ? array_meta(array)->size = 0 : 0)
 
-#define array_df(X) X* __attribute__((cleanup(array_defer)))
-
-__attribute__((__noreturn__)) void report_assertion(const char *expression, const char *file, int line);
 bool array_grow(void **const array, size_t elements, size_t type_size);
 void array_delete(void **const array);
 void *array_create_init(size_t capacity, size_t type_size);
 
 #ifdef ARRAY_IMPLEMENTATION
 
-__attribute__((unused)) static void array_defer(void *array) {
-    array_free(*(void**)array);
-}
-
-__attribute__((__noreturn__)) void report_assertion(const char *expression, const char *file, int line) {
-    fprintf(stderr, "%s:%d: Assertion failed on %s\n", file, line, expression);
-    abort();
-}
-
+/**
+ * @brief Agrandit la capacité d'un tableau dynamique.
+ * 
+ * Cette fonction augmente la capacité d'un tableau dynamique afin de pouvoir
+ * contenir un certain nombre d'éléments supplémentaires. Elle est utilisée
+ * par les macros de gestion de tableau pour redimensionner le tableau au besoin.
+ *
+ * @param array   Un pointeur vers le tableau à agrandir.
+ * @param elements Le nombre d'éléments supplémentaires à prendre en compte.
+ * @param type_size La taille en octets d'un élément du tableau.
+ * @return Retourne true si l'agrandissement réussit, false en cas d'échec.
+ */
 bool array_grow(void **const array, size_t elements, size_t type_size) {
-    ASSERT(*array);
+    assert(*array);
     Array *meta = array_meta(*array);
     const size_t count = 2 * meta->capacity + elements;
     void *data = realloc(meta, type_size * count + sizeof *meta);
@@ -116,12 +163,32 @@ bool array_grow(void **const array, size_t elements, size_t type_size) {
     return true;
 }
 
+/**
+ * @brief Libère la mémoire associée à un tableau dynamique.
+ * 
+ * Cette fonction libère la mémoire allouée pour un tableau dynamique,
+ * y compris les métadonnées stockées avant le début du tableau.
+ *
+ * @param array Un pointeur vers le tableau à libérer.
+ */
 void array_delete(void **const array) {
-    ASSERT(array);
+    assert(array);
     Array *const meta = array_meta(array);
     free(meta);
 }
 
+/**
+ * @brief Crée et initialise un nouveau tableau dynamique.
+ * 
+ * Cette fonction alloue la mémoire nécessaire pour un nouveau tableau
+ * dynamique et initialise ses métadonnées, puis renvoie un pointeur
+ * vers la première position du tableau.
+ *
+ * @param capacity La capacité initiale du tableau (nombre d'éléments pouvant être stockés).
+ * @param type_size La taille en octets d'un élément du tableau.
+ * @return Un pointeur vers la première position du tableau nouvellement créé.
+ *         En cas d'échec, renvoie NULL.
+ */
 void *array_create_init(size_t capacity, size_t type_size) {
     void *data = malloc(type_size * capacity + sizeof(Array));
     if (!data) {
--- src/main.c
+++ src/main.c
@@ -12,7 +12,10 @@
 const char *libplug_file_name = "libplug.so";
 void *libplug = NULL;
 
+// macro utilisé pour définir une fonction de plug (avec ou sans hotreload)
 #define BASE_PLUG(return_type, name, ...) PLUG(name)
+// Si le hotreload est activé, les fonctions de plug sont déclarées comme des pointeurs.
+// Sinon, elles sont déclarées directement comme des objets.
 #ifdef HOTRELOAD
 #  define PLUG(name) name##_t *name = NULL;
 #else
@@ -22,18 +25,31 @@ LIST_OF_PLUGS
 #undef PLUG
 #undef BASE_PLUG
 
+// instance de la structure Plug initialisée à zéro
 Plug plug = {0};
 
 #ifdef HOTRELOAD
+/**
+ * @brief Recharge la bibliothèque dynamique libplug.so pour le hot-reloading.
+ *
+ * Si HOTRELOAD est activé, cette fonction ferme la bibliothèque précédemment chargée,
+ * charge la nouvelle bibliothèque libplug.so, et redéfinit les fonctions du plug
+ * avec leurs nouvelles adresses. Si HOTRELOAD n'est pas activé, cette fonction renvoie toujours true.
+ *
+ * @return Retourne true si le rechargement est réussi ou si HOTRELOAD n'est pas activé, false sinon.
+ */
 bool reload_libplug(void) {
+    // Fermeture de la librairie précédemment chargée, si elle existe
     if (libplug != NULL) dlclose(libplug);
 
+    // Chargement de la nouvelle librairie libplug.so
     libplug = dlopen(libplug_file_name, RTLD_NOW);
     if (libplug == NULL) {
 	fprintf(stderr, "ERROR: could not load %s: %s", libplug_file_name, dlerror());
 	return false;
     }
 
+    // Redéfinition des fonctions de plug avec les nouvelles adresses après le chargement.
 #define BASE_PLUG(return_type, name, ...) PLUG(name)
 #define PLUG(name) \
     name = dlsym(libplug, #name); \
@@ -48,6 +64,7 @@ bool reload_libplug(void) {
     return true;
 }
 #else
+// Si le hotreload n'est pas activé, cette fonction retourne toujours true
 #define reload_libplug() true
 #endif // HOTRELOAD
 
@@ -57,10 +74,11 @@ int main(void) {
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lemmings");
 
+    // Chargement de tout les textures pour le jeu
     Texture2D background = LoadTexture("./assets/pixel-platformer/Tilemap/newtest.png");
-    Texture2D demoTile = LoadTexture("./assets/pixel-platformer/Tilemap/scaled_packed.png");
+    Texture2D tileset = LoadTexture("./assets/pixel-platformer/Tilemap/scaled_packed.png");
     Texture2D player = LoadTexture("./assets/pixel-platformer/Tilemap/tilemap-characters_scaled.png");
-    Texture2D menu = LoadTexture("./assets/ui-pack-space/Spritesheet/uipackSpace_sheet.png");
+    Texture2D player_flop = LoadTexture("./assets/pixel-platformer/Tilemap/tilemap-characters_scaled_flop.png");
 
     SetTargetFPS(60);
 
@@ -72,21 +90,19 @@ int main(void) {
 
 	if (IsKeyPressed(KEY_Z)) plug.camera.zoom = 1.0f;
 
-	if (IsKeyPressed(KEY_R)) {
+	if (IsKeyPressed(KEY_R) && plug.dialog == DIALOG_NONE) {
 	    if (!reload_libplug()) return 1;
 	}
 
-	if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_S)) {
-	    plug_save(&plug);
-	}
-
-	plug_render(&plug, background, demoTile, player, menu);
+	plug_render(&plug, background, tileset, player, player_flop);
     }
 
     plug_free(&plug);
 
     UnloadTexture(background);
-    UnloadTexture(demoTile);
+    UnloadTexture(tileset);
+    UnloadTexture(player);
+    UnloadTexture(player_flop);
     CloseWindow();
 
     return 0;
