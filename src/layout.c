04930ab5fc7d11bad7696acb0e355c2c3952bac1
Spad0n <spad0n.chairmanship@simplelogin.co>
2023-12-17 19:31:06 +0100

    renommage des fonctions dans les fichiers layout.c et layout.h

--- src/layout.c
+++ src/layout.c
@@ -1,6 +1,6 @@
 /* -*- compile-command: "make -C .. libplug" -*- */
 #include "raylib.h"
-#include "ui.h"
+#include "layout.h"
 #include <math.h>
 #include <assert.h>
 #include <stdio.h>
@@ -10,7 +10,7 @@
 
 #define DRAW_TEXTURE_REC(texture, source, rec, color) DrawTextureRec((texture), (source), (Vector2){(rec).x + x, (rec).y + y}, (color))
 
-Rectangle ui_make_layout_rec(float x, float y, float w, float h) {
+Rectangle layout_make_rec(float x, float y, float w, float h) {
     Rectangle r = {
 	.x = x,
 	.y = y,
@@ -20,81 +20,68 @@ Rectangle ui_make_layout_rec(float x, float y, float w, float h) {
     return r;
 }
 
-void ui_widget(Rectangle rect, Color color) {
-    Rectangle rr = {
-	ceilf(rect.x), ceilf(rect.y), ceilf(rect.width), ceilf(rect.height)
-    };
-    if (!CheckCollisionPointRec(GetMousePosition(), rr)) {
-	color = Fade(color, 0.5f);
-    }
-    DrawRectangleRec(rr, color);
-}
+//void ui_widget(Rectangle rect, Color color) {
+//    Rectangle rr = {
+//	ceilf(rect.x), ceilf(rect.y), ceilf(rect.width), ceilf(rect.height)
+//    };
+//    if (!CheckCollisionPointRec(GetMousePosition(), rr)) {
+//	color = Fade(color, 0.5f);
+//    }
+//    DrawRectangleRec(rr, color);
+//}
+//
+//void ui_widget_button(Texture2D texture, Rectangle rect, const char *text) {
+//    Rectangle rr = {
+//	ceilf(rect.x), ceilf(rect.y), ceilf(rect.width), ceilf(rect.height)
+//    };
+//    Color color = WHITE;
+//
+//    Rectangle recs[9] = {
+//	BUTTON_PANEL_TOP_LEFT,
+//	BUTTON_PANEL_TOP,
+//	BUTTON_PANEL_TOP_RIGHT,
+//	BUTTON_PANEL_LEFT,
+//	BUTTON_PANEL_MIDDLE,
+//	BUTTON_PANEL_RIGHT,
+//	BUTTON_PANEL_BOTTOM_LEFT,
+//	BUTTON_PANEL_BOTTOM,
+//	BUTTON_PANEL_BOTTOM_RIGHT,
+//    };
+//
+//    size_t tile_width = UI_TILE_SIZE * (int)(rr.width / UI_TILE_SIZE);
+//    rr.x = rr.x + (rr.width - tile_width) / 2;
+//    rr.width = tile_width;
+//    size_t tile_height = UI_TILE_SIZE * (int)(rr.height / UI_TILE_SIZE);
+//    rr.y = rr.y + (rr.height - tile_height) / 2;
+//    rr.height = tile_height;
+//
+//    if (!CheckCollisionPointRec(GetMousePosition(), rr)) {
+//	color = Fade(color, 0.5f);
+//    }
+//
+//    for (size_t y = 0; y < tile_height; y += UI_TILE_SIZE) {
+//	for (size_t x = 0; x < tile_width; x += UI_TILE_SIZE) {
+//	    if (x == 0 && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_LEFT], rr, color);
+//	    else if (x == 0 && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_LEFT], rr, color);
+//	    else if (x == tile_width - UI_TILE_SIZE && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_RIGHT], rr, color);
+//	    else if (x == tile_width - UI_TILE_SIZE && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_RIGHT], rr, color);
+//	    else if (y == 0) DRAW_TEXTURE_REC(texture, recs[TOP], rr, color);
+//	    else if (x == 0) DRAW_TEXTURE_REC(texture, recs[LEFT], rr, color);
+//	    else if (y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM], rr, color);
+//	    else if (x == tile_width - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[RIGHT], rr, color);
+//	    else DRAW_TEXTURE_REC(texture, recs[MIDDLE], rr, color);
+//	}
+//    }
+//
+//    Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 3);
+//    Vector2 text_pos = {
+//	.x = rr.x + (tile_width - text_size.x) / 2,
+//	.y = rr.y + (tile_height - text_size.y) / 2,
+//    };
+//    DrawTextEx(GetFontDefault(), text, text_pos, 20, 3, color);
+//}
 
-void ui_widget_button(Texture2D texture, Rectangle rect, const char *text) {
-    Rectangle rr = {
-	ceilf(rect.x), ceilf(rect.y), ceilf(rect.width), ceilf(rect.height)
-    };
-    Color color = WHITE;
-
-    Rectangle recs[9] = {
-	BUTTON_PANEL_TOP_LEFT,
-	BUTTON_PANEL_TOP,
-	BUTTON_PANEL_TOP_RIGHT,
-	BUTTON_PANEL_LEFT,
-	BUTTON_PANEL_MIDDLE,
-	BUTTON_PANEL_RIGHT,
-	BUTTON_PANEL_BOTTOM_LEFT,
-	BUTTON_PANEL_BOTTOM,
-	BUTTON_PANEL_BOTTOM_RIGHT,
-    };
-
-    size_t tile_width = UI_TILE_SIZE * (int)(rr.width / UI_TILE_SIZE);
-    rr.x = rr.x + (rr.width - tile_width) / 2;
-    rr.width = tile_width;
-    size_t tile_height = UI_TILE_SIZE * (int)(rr.height / UI_TILE_SIZE);
-    rr.y = rr.y + (rr.height - tile_height) / 2;
-    rr.height = tile_height;
-
-    if (!CheckCollisionPointRec(GetMousePosition(), rr)) {
-	color = Fade(color, 0.5f);
-    }
-
-    for (size_t y = 0; y < tile_height; y += UI_TILE_SIZE) {
-	for (size_t x = 0; x < tile_width; x += UI_TILE_SIZE) {
-	    if (x == 0 && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_LEFT], rr, color);
-	    else if (x == 0 && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_LEFT], rr, color);
-	    else if (x == tile_width - UI_TILE_SIZE && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_RIGHT], rr, color);
-	    else if (x == tile_width - UI_TILE_SIZE && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_RIGHT], rr, color);
-	    else if (y == 0) DRAW_TEXTURE_REC(texture, recs[TOP], rr, color);
-	    else if (x == 0) DRAW_TEXTURE_REC(texture, recs[LEFT], rr, color);
-	    else if (y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM], rr, color);
-	    else if (x == tile_width - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[RIGHT], rr, color);
-	    else DRAW_TEXTURE_REC(texture, recs[MIDDLE], rr, color);
-	}
-    }
-
-    Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 3);
-    Vector2 text_pos = {
-	.x = rr.x + (tile_width - text_size.x) / 2,
-	.y = rr.y + (tile_height - text_size.y) / 2,
-    };
-    DrawTextEx(GetFontDefault(), text, text_pos, 20, 3, color);
-}
-
-void ui_widget_item(Texture2D texture, Rectangle rect, Rectangle source) {
-
-    //size_t tile_width = UI_TILE_SIZE * (int)(rect.width / UI_TILE_SIZE);
-    //rect.x = rect.x + (rect.width - tile_width) / 2;
-    //rect.width = tile_width;
-    //size_t tile_height = UI_TILE_SIZE * (int)(rect.height / UI_TILE_SIZE);
-    //rect.y = rect.y + (rect.height - tile_height) / 2;
-    //rect.height = tile_height;
-
-    //rect.x = ceilf(rect.x + (rect.width - source.width) / 2);
-    //rect.y = ceilf(rect.y + (rect.height - source.height) / 2);
-    //rect.width = source.width;
-    //rect.height = source.height;
-
+void layout_item(Texture2D texture, Rectangle rect, Rectangle source) {
     Color color = WHITE;
     if (!CheckCollisionPointRec(GetMousePosition(), rect)) {
 	color = Fade(color, 0.5f);
@@ -103,16 +90,16 @@ void ui_widget_item(Texture2D texture, Rectangle rect, Rectangle source) {
     DrawTexturePro(texture, source, rect, (Vector2){0, 0}, 0, color);
 }
 
-void ui_widget_text(Rectangle rect, const char *text, Color color) {
-    Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 3);
-    Vector2 text_pos = {
-	.x = rect.x + (rect.width - text_size.x) / 2,
-	.y = rect.y + (rect.height - text_size.y) / 2,
-    };
-    DrawTextEx(GetFontDefault(), text, text_pos, 20, 3, color);
-}
+//void ui_widget_text(Rectangle rect, const char *text, Color color) {
+//    Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 3);
+//    Vector2 text_pos = {
+//	.x = rect.x + (rect.width - text_size.x) / 2,
+//	.y = rect.y + (rect.height - text_size.y) / 2,
+//    };
+//    DrawTextEx(GetFontDefault(), text, text_pos, 20, 3, color);
+//}
 
-Layout ui_make_layout(Layout_Orient orient, Rectangle rect, size_t count, float gap) {
+Layout layout_make(Layout_Orient orient, Rectangle rect, size_t count, float gap) {
     Layout result = {
 	.orient = orient,
 	.rect = rect,
@@ -122,7 +109,7 @@ Layout ui_make_layout(Layout_Orient orient, Rectangle rect, size_t count, float 
     return result;
 }
 
-static Rectangle ui_layout_slot(Layout *l, const char *file_path, int line) {
+static Rectangle layout_slot(Layout *l, const char *file_path, int line) {
     if (l->i >= l->count) {
 	fprintf(stderr, "%s:%d: ERROR: Layout overflow\n", file_path, line);
 	exit(1);
@@ -172,70 +159,70 @@ static Rectangle ui_layout_slot(Layout *l, const char *file_path, int line) {
     return r;
 }
 
-void ui_layout_stack_push(Layout **ls, Layout_Orient orient, Rectangle rect, size_t count, float gap) {
-    Layout l = ui_make_layout(orient, rect, count, gap);
+void layout_stack_push(Layout **ls, Layout_Orient orient, Rectangle rect, size_t count, float gap) {
+    Layout l = layout_make(orient, rect, count, gap);
     array_push(*ls, l);
 }
 
-void ui_layout_stack_pop(Layout **ls) {
+void layout_stack_pop(Layout **ls) {
     assert(array_size(*ls) > 0);
     array_pop_last(*ls);
 }
 
 Rectangle layout_stack_slot_loc(Layout **ls, const char *file_path, int line) {
     assert(array_size(*ls) > 0);
-    return ui_layout_slot(&array_last(*ls), file_path, line);
+    return layout_slot(&array_last(*ls), file_path, line);
 }
 
-void ui_draw_dialog(Rectangle rec, Texture2D texture, PanelType panel_type, Color color) {
-
-    Rectangle recs[9] = {
-	GLASS_PANEL_TOP_LEFT,
-	GLASS_PANEL_TOP,
-	GLASS_PANEL_TOP_RIGHT,
-	GLASS_PANEL_LEFT,
-	GLASS_PANEL_MIDDLE,
-	GLASS_PANEL_RIGHT,
-	GLASS_PANEL_BOTTOM_LEFT,
-	GLASS_PANEL_BOTTOM,
-	GLASS_PANEL_BOTTOM_RIGHT,
-    };
-
-    switch (panel_type) {
-    case GlassCorner:
-	recs[TOP_LEFT] = GLASS_PANEL_CORNER_TOP_LEFT;
-	recs[TOP_RIGHT] = GLASS_PANEL_CORNER_TOP_RIGHT;
-	recs[BOTTOM_LEFT] = GLASS_PANEL_CORNER_BOTTOM_LEFT;
-	recs[BOTTOM_RIGHT] = GLASS_PANEL_CORNER_BOTTOM_RIGHT;
-	break;
-    case Metal:
-	recs[TOP_LEFT] = METAL_PANEL_TOP_LEFT;
-	recs[TOP] = METAL_PANEL_TOP;
-	recs[TOP_RIGHT] = METAL_PANEL_TOP_RIGHT;
-	recs[LEFT] = METAL_PANEL_LEFT;
-	recs[MIDDLE] = METAL_PANEL_MIDDLE;
-	recs[RIGHT] = METAL_PANEL_RIGHT;
-	recs[BOTTOM_LEFT] = METAL_PANEL_BOTTOM_LEFT;
-	recs[BOTTOM] = METAL_PANEL_BOTTOM;
-	recs[BOTTOM_RIGHT] = METAL_PANEL_BOTTOM_RIGHT;
-	break;
-    default:
-    }
-
-    size_t tile_height = UI_TILE_SIZE * (int)(rec.height / UI_TILE_SIZE);
-    size_t tile_width = UI_TILE_SIZE * (int)(rec.width / UI_TILE_SIZE);
-
-    for (size_t y = 0; y < tile_height; y += UI_TILE_SIZE) {
-	for (size_t x = 0; x < tile_width; x += UI_TILE_SIZE) {
-	    if (x == 0 && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_LEFT], rec, color);
-	    else if (x == 0 && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_LEFT], rec, color);
-	    else if (x == tile_width - UI_TILE_SIZE && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_RIGHT], rec, color);
-	    else if (x == tile_width - UI_TILE_SIZE && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_RIGHT], rec, color);
-	    else if (y == 0) DRAW_TEXTURE_REC(texture, recs[TOP], rec, color);
-	    else if (x == 0) DRAW_TEXTURE_REC(texture, recs[LEFT], rec, color);
-	    else if (y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM], rec, color);
-	    else if (x == tile_width - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[RIGHT], rec, color);
-	    else DRAW_TEXTURE_REC(texture, recs[MIDDLE], rec, color);
-	}
-    }
-}
+//void ui_draw_dialog(Rectangle rec, Texture2D texture, PanelType panel_type, Color color) {
+//
+//    Rectangle recs[9] = {
+//	GLASS_PANEL_TOP_LEFT,
+//	GLASS_PANEL_TOP,
+//	GLASS_PANEL_TOP_RIGHT,
+//	GLASS_PANEL_LEFT,
+//	GLASS_PANEL_MIDDLE,
+//	GLASS_PANEL_RIGHT,
+//	GLASS_PANEL_BOTTOM_LEFT,
+//	GLASS_PANEL_BOTTOM,
+//	GLASS_PANEL_BOTTOM_RIGHT,
+//    };
+//
+//    switch (panel_type) {
+//    case GlassCorner:
+//	recs[TOP_LEFT] = GLASS_PANEL_CORNER_TOP_LEFT;
+//	recs[TOP_RIGHT] = GLASS_PANEL_CORNER_TOP_RIGHT;
+//	recs[BOTTOM_LEFT] = GLASS_PANEL_CORNER_BOTTOM_LEFT;
+//	recs[BOTTOM_RIGHT] = GLASS_PANEL_CORNER_BOTTOM_RIGHT;
+//	break;
+//    case Metal:
+//	recs[TOP_LEFT] = METAL_PANEL_TOP_LEFT;
+//	recs[TOP] = METAL_PANEL_TOP;
+//	recs[TOP_RIGHT] = METAL_PANEL_TOP_RIGHT;
+//	recs[LEFT] = METAL_PANEL_LEFT;
+//	recs[MIDDLE] = METAL_PANEL_MIDDLE;
+//	recs[RIGHT] = METAL_PANEL_RIGHT;
+//	recs[BOTTOM_LEFT] = METAL_PANEL_BOTTOM_LEFT;
+//	recs[BOTTOM] = METAL_PANEL_BOTTOM;
+//	recs[BOTTOM_RIGHT] = METAL_PANEL_BOTTOM_RIGHT;
+//	break;
+//    default:
+//    }
+//
+//    size_t tile_height = UI_TILE_SIZE * (int)(rec.height / UI_TILE_SIZE);
+//    size_t tile_width = UI_TILE_SIZE * (int)(rec.width / UI_TILE_SIZE);
+//
+//    for (size_t y = 0; y < tile_height; y += UI_TILE_SIZE) {
+//	for (size_t x = 0; x < tile_width; x += UI_TILE_SIZE) {
+//	    if (x == 0 && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_LEFT], rec, color);
+//	    else if (x == 0 && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_LEFT], rec, color);
+//	    else if (x == tile_width - UI_TILE_SIZE && y == 0) DRAW_TEXTURE_REC(texture, recs[TOP_RIGHT], rec, color);
+//	    else if (x == tile_width - UI_TILE_SIZE && y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM_RIGHT], rec, color);
+//	    else if (y == 0) DRAW_TEXTURE_REC(texture, recs[TOP], rec, color);
+//	    else if (x == 0) DRAW_TEXTURE_REC(texture, recs[LEFT], rec, color);
+//	    else if (y == tile_height - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[BOTTOM], rec, color);
+//	    else if (x == tile_width - UI_TILE_SIZE) DRAW_TEXTURE_REC(texture, recs[RIGHT], rec, color);
+//	    else DRAW_TEXTURE_REC(texture, recs[MIDDLE], rec, color);
+//	}
+//    }
+//}
--- src/layout.h
+++ src/layout.h
@@ -4,67 +4,11 @@
 #include <stddef.h>
 #include "raylib.h"
 
-#define UI_TILE_SIZE 33
+#define LayoutDrawing(layout, orient, layout_rect, count, gap)	\
+    for (int _break = (layout_stack_push((layout), (orient), (layout_rect), (count), (gap)), 1); _break; \
+	 _break = 0, layout_stack_pop(layout))
 
-#define GLASS_PANEL_TOP_LEFT (Rectangle){0, 0, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_TOP (Rectangle){UI_TILE_SIZE, 0, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_TOP_RIGHT (Rectangle){UI_TILE_SIZE * 2, 0, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_LEFT (Rectangle){0, UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_MIDDLE (Rectangle){UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_RIGHT (Rectangle){UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_BOTTOM_LEFT (Rectangle){0, UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_BOTTOM (Rectangle){UI_TILE_SIZE, UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_BOTTOM_RIGHT (Rectangle){UI_TILE_SIZE * 2, UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-
-#define GLASS_PANEL_CORNER_TOP_LEFT (Rectangle){100, 200, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_CORNER_TOP_RIGHT (Rectangle){100 + UI_TILE_SIZE * 2, 200, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_CORNER_BOTTOM_LEFT (Rectangle){100, 200 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define GLASS_PANEL_CORNER_BOTTOM_RIGHT (Rectangle){100 + UI_TILE_SIZE * 2, 200 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-
-#define METAL_PANEL_TOP_LEFT (Rectangle){200, 200, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_TOP (Rectangle){200 + UI_TILE_SIZE, 200, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_TOP_RIGHT (Rectangle){200 + UI_TILE_SIZE * 2, 200, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_LEFT (Rectangle){200, 200 + UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_MIDDLE (Rectangle){200 + UI_TILE_SIZE, 200 + UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_RIGHT (Rectangle){200 + UI_TILE_SIZE * 2, 200 + UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_BOTTOM_LEFT (Rectangle){200, 200 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_BOTTOM (Rectangle){200 + UI_TILE_SIZE, 200 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define METAL_PANEL_BOTTOM_RIGHT (Rectangle){200 + UI_TILE_SIZE * 2, 200 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-
-#define BUTTON_PANEL_TOP_LEFT (Rectangle){200, 300, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_TOP (Rectangle){200 + UI_TILE_SIZE, 300, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_TOP_RIGHT (Rectangle){200 + UI_TILE_SIZE * 2, 300, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_LEFT (Rectangle){200, 300 + UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_MIDDLE (Rectangle){200 + UI_TILE_SIZE, 300 + UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_RIGHT (Rectangle){200 + UI_TILE_SIZE * 2, 300 + UI_TILE_SIZE, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_BOTTOM_LEFT (Rectangle){200, 300 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_BOTTOM (Rectangle){200 + UI_TILE_SIZE, 300 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-#define BUTTON_PANEL_BOTTOM_RIGHT (Rectangle){200 + UI_TILE_SIZE * 2, 300 + UI_TILE_SIZE * 2, UI_TILE_SIZE, UI_TILE_SIZE}
-
-#define UILayoutDrawing(layout, orient, layout_rect, count, gap)	\
-    for (int _break = (ui_layout_stack_push((layout), (orient), (layout_rect), (count), (gap)), 1); _break; \
-	 _break = 0, ui_layout_stack_pop(layout))
-
-#define ui_layout_stack_slot(ls) layout_stack_slot_loc(ls, __FILE__, __LINE__)
-
-typedef enum {
-    GlassCorner,
-    Glass,
-    Metal,
-    Button,
-} PanelType;
-
-typedef enum {
-    TOP_LEFT,
-    TOP,
-    TOP_RIGHT,
-    LEFT,
-    MIDDLE,
-    RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM,
-    BOTTOM_RIGHT,
-} TextureIndex;
+#define layout_stack_slot(ls) layout_stack_slot_loc(ls, __FILE__, __LINE__)
 
 typedef enum {
     LO_HORI,
@@ -79,25 +23,16 @@ typedef struct {
     float gap;
 } Layout;
 
-//void ui_widget(Texture2D texture, Rectangle source, Vector2 vector);
-void ui_widget(Rectangle rect, Color color);
+void layout_item(Texture2D texture, Rectangle rect, Rectangle source);
 
-void ui_widget_text(Rectangle rect, const char* text, Color color);
+Rectangle layout_make_rec(float x, float y, float w, float h);
 
-void ui_widget_button(Texture2D texture, Rectangle rect, const char *text);
+Layout layout_make(Layout_Orient orient, Rectangle rect, size_t count, float gap);
 
-void ui_widget_item(Texture2D texture, Rectangle rect, Rectangle source);
+void layout_stack_push(Layout **ls, Layout_Orient orient, Rectangle rect, size_t count, float gap);
 
-Rectangle ui_make_layout_rec(float x, float y, float w, float h);
-
-Layout ui_make_layout(Layout_Orient orient, Rectangle rect, size_t count, float gap);
-
-void ui_layout_stack_push(Layout **ls, Layout_Orient orient, Rectangle rect, size_t count, float gap);
-
-void ui_layout_stack_pop(Layout **ls);
+void layout_stack_pop(Layout **ls);
 
 Rectangle layout_stack_slot_loc(Layout **ls, const char *file_path, int line);
-
-void ui_draw_dialog(Rectangle rec, Texture2D texture, PanelType panel_type, Color color);
 
 #endif // UI_H_
